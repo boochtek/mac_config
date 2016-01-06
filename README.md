db76d5e450555db1d897bae7446932cb69b567c4
Craig Buchek <craig@boochtek.com>
2016-01-06 15:30:20 -0600

    Initial Ansible implementation
    Got Ansible started with a playbook and a few roles.
    
    Created a custom dynamic inventory script to make it easy to determine
    what computer we're running on (by MAC address). This will let us use
    a simple config file to decide how to configure each computer, and we
    won't need to specify anything system-specific at the command line.
    
    Note that we need at least Ansible 2.0 to run the osx_defaults module.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 Mac OS X Configuration
 ======================
 
+NOTE: We're in the process of moving to Ansible.
+These instructions are currently in transition as well.
+
 These scripts will configure Mac OS X the way we want, including:
 
   * Configuring OS preferences
   * Installing applications
   * Configuring applications
 
-These scripts are written for Mac OS X 10.9 (Mavericks).
-The scripts should all be idempotent.
-(That means that you can run them as many times as you want.)
+These scripts are written for Mac OS X 10.11 (El Capitan).
 
 
 Usage
@@ -34,4 +35,13 @@ cd mac_config
 ./ruby.sh
 ./java.sh
 ./terminal.sh   # NOTE: This will kill Terminal when it's done.
+~~~
+
+
+Ansible
+-------
+
+~~~ shell
+cd mac_config
+ansible2-playbook mac.yml # Requires Ansible 2.0
 ~~~
---
+++ inventory_for_current_mac_address.sh
@@ -0,0 +1,76 @@
+#!/bin/bash -e
+
+
+# Returns the hostname and groups for the local (Mac) system, based on MAC address.
+# Used as a dynamic inventory script for Ansible.
+# See http://docs.ansible.com/ansible/developing_inventory.html for details.
+
+
+# TODO:
+#   * Return an empty hash if given --host
+#   * Error out on anything other than --host or --list or --all
+#   * Move configuration to an external file.
+
+
+# Mappings of MAC address to hostname and groups.
+HOST_MAPPINGS="
+a0:99:9b:00:00:00  hope      mac workstation dev mercy
+a0:99:9b:03:8d:9b  big_bang  mac workstation dev centurylink
+"
+
+
+function main() {
+    local hostname=$(hostname)
+    local groups="$(groups_for_host $hostname)"
+    echo "$(list_json $hostname "$groups")"
+}
+
+
+function hostname() {
+  local hostname=""
+
+  for mac_address in "$(mac_addresses)" ; do
+      hostname="$(hostname_for_mac_address $mac_address)"
+      [[ ! -z $hostname ]] && echo $hostname && return 0
+  done
+
+  return 1
+}
+
+
+function groups_for_host() {
+    local hostname=$1
+    grep $hostname <<<"$HOST_MAPPINGS" | awk '{$1 = ""; $2 = ""; print $0}'
+}
+
+
+function list_json() {
+    local hostname=$1
+    local groups="$2"
+    local first_group=0
+
+    echo "{"
+    for group in $groups ; do
+        if [[ $first_group -eq 0 ]]; then
+            first_group=1
+        else
+            echo ","
+        fi
+        echo "  \"$group\": [ \"$hostname\" ]"
+    done
+    echo "}"
+}
+
+
+function mac_addresses() {
+    ifconfig | grep -E "^\s+ether\s+([0-9a-f]{2}:){5}([0-9a-f]{2})" | awk '{print $2}'
+}
+
+
+function hostname_for_mac_address() {
+    local mac_address=$1
+    grep $mac_address <<<"$HOST_MAPPINGS" | awk '{print $2}'
+}
+
+
+main
