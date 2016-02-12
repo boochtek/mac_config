5e9d6c263bf5e73031050eb81c203e56db4c6803
Craig Buchek <craig@boochtek.com>
2016-02-11 22:25:15 -0600

    Update Ruby installation
    * Install latest versions.
    * Add Ruby 2.2 and 2.3.
    * Install a few gems only in latest Ruby version.

--- ruby.sh
+++ ruby.sh
@@ -10,11 +10,18 @@ source 'homebrew.sh'
 
 # Install various versions of Ruby.
 brew install ruby-install
-ruby-install --no-reinstall ruby 1.9.3
-ruby-install --no-reinstall ruby 2.0
-ruby-install --no-reinstall ruby 2.1
-ruby-install --no-reinstall rbx 2.2
-ruby-install --no-reinstall jruby 1.7
+brew update
+brew upgrade ruby-install
+ruby-install ruby 1.9.3
+ruby-install ruby 2.0
+ruby-install ruby 2.0.0-p648 --sha1 504be2eae6cdfe93aa7ed02ec55e35043d067ad5
+ruby-install ruby 2.1
+ruby-install ruby 2.1.8 --sha1 0284eaf42ac76a641abd5cd4b6bd933abeb9934e
+ruby-install ruby 2.2
+ruby-install ruby 2.2.4 --sha1 6132840a859dbf2ac1498ba313021f299a870038
+ruby-install ruby 2.3.0 --sha1 6c8a832d49d22755ea8c45b6d53faf2ccc6d6ac9
+ruby-install rbx 2.5
+ruby-install jruby 1.7
 
 # If you need Ruby 1.9.3-p125:
 #brew install gcc46
@@ -34,19 +41,19 @@ source /usr/local/share/chruby/auto.sh
 # Ensure new bash shells use chruby automatically.
 grep -q 'chruby' ~/.bashrc || cat >> ~/.bashrc <<'EOF'
 
-# Automatically change Ruby versions when changing directories. Set a default Ruby version.
-if [[ -d /usr/local/share/chruby ]]; then
-    source /usr/local/share/chruby/chruby.sh
-    source /usr/local/share/chruby/auto.sh
-    chruby 2.1
-fi
+# Use chruby automatically.
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+
+# Set a default Ruby version. FIXME/TODO: Should probably just create a ~/.ruby-version file.
+chruby 2.3
 EOF
 
 
 ## Gems
 
 # Install some gems for every version of Ruby. These are either for irb or for the commands they provide,
-GEMS_TO_INSTALL='rake bundler awesome_print hirb wirble pry rails haml html2haml'
+GEMS_TO_INSTALL='bundler awesome_print hirb wirble pry railties rubocop'
 ALL_RUBY_VERSIONS=$(chruby | sed -e 's/*//' | awk '{print $1}')
 CURRENT_RUBY_VERSION=$(chruby | grep '*' | awk '{print $2}' | tr -d '\n')
 if [[ -z "$CURRENT_RUBY_VERSION" ]]; then
@@ -55,6 +62,17 @@ fi
 for ruby_version in $ALL_RUBY_VERSIONS ; do
     chruby $ruby_version
     echo "Installing gems into $ruby_version: $GEMS_TO_INSTALL"
+    gem update --system
+    gem update bundler
     gem install $GEMS_TO_INSTALL
 done
 chruby $CURRENT_RUBY_VERSION
+
+
+# Install some gems for ONLY the latest Ruby.
+gem install kramdown
+gem install rsense
+gem install ruby-beautify
+brew cask install wkhtmltopdf
+gem install middleman middleman-presentation
+npm install -g bower # Needed by Middleman-Presentation
