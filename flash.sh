b35cd50d10feaedcd0ae466fe021afda60efa3b3
Craig Buchek <craig@boochtek.com>
2016-02-11 22:44:24 -0600

    Install Flash Player via Homebrew Cask

---
+++ flash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Install Adobe Flash Player.
+
+source 'homebrew.sh'
+
+
+# Install Flash Player plugin.
+# NOTE: There's another cask called `flash-player` that duplicates this functionality.
+brew cask install flash
