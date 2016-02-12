99c78669535d84fff1f3e2984a8e026b4b6655ab
Craig Buchek <craig@boochtek.com>
2016-02-11 23:04:34 -0600

    Document manual configuration of Postbox email client

---
+++ postbox.rb
@@ -0,0 +1,8 @@
+# Manual config:
+#
+#   Import settings from Thunderbird
+#   View / Layout / Classic View
+#   View / Sort By / Date
+#   View / Sort By / Ascending
+#   Right-click message list headers / CHECK Junk Status
+#   Preferences / Accounts / (each account) / Composition / UNCHECK Compose messages in HTML format
