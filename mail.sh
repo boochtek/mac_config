7a6e062b81cd4b02b0c659e11e2e4f44df544486
Craig Buchek <craig@boochtek.com>
2016-02-11 22:59:16 -0600

    Configure some Mail.app settings

---
+++ mail.sh
@@ -0,0 +1,7 @@
+# mail.sh
+
+## Applications / Mail
+
+defaults write com.apple.mail 'PreferPlainText' -bool true
+defaults write com.apple.mail 'DisableReplyAnimations' -bool true
+defaults write com.apple.mail 'DisableSendAnimations' -bool true
