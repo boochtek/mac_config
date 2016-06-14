2965e9816608e4ffcb0a078492f3d4f885c8972e
Craig Buchek <craig@boochtek.com>
2016-06-14 13:09:51 -0500

    Enable bash completion for Vagrant
    Also fix Terraform Inventory git URL.

--- mail.sh
+++
@@ -1,7 +0,0 @@
-# mail.sh
-
-## Applications / Mail
-
-defaults write com.apple.mail 'PreferPlainText' -bool true
-defaults write com.apple.mail 'DisableReplyAnimations' -bool true
-defaults write com.apple.mail 'DisableSendAnimations' -bool true
