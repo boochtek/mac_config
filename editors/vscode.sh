5053f6eb87fb87838138bfa2ebddc540329749f6
Craig Buchek <craig@boochtek.com>
2022-02-23 12:56:01 -0800

    Install VS Code and a bunch of plugins I find useful

---
+++ editors/vscode.sh
@@ -0,0 +1,102 @@
+brew install --cask visual-studio-code
+
+# TODO: Check `code --list-extensions` before trying to install each extension.
+
+# Misc.
+code --install-extension pdconsec.vscode-print # Better of the 2 printing extensions.
+code --install-extension ban.spellright
+code --install-extension Tyriar.sort-lines
+code --install-extension 2gua.rainbow-brackets # TODO: I think this functionality is included in VS Code by default now.
+code --install-extension CoenraadS.bracket-pair-colorizer-2 # TODO: "     "
+code --install-extension alefragnani.project-manager
+code --install-extension christian-kohler.path-intellisense
+code --install-extension lostintangent.vsls-whiteboard
+code --install-extension thibaudcolas.stylelint
+
+# Git and GitHub
+code --install-extension codezombiech.gitignore
+code --install-extension donjayamanne.git-extension-pack
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension ziyasal.vscode-open-in-github
+
+# Markdown
+code --install-extension bierner.github-markdown-preview
+code --install-extension bierner.markdown-checkbox
+code --install-extension bierner.markdown-emoji
+code --install-extension bierner.markdown-preview-github-styles
+code --install-extension bierner.markdown-yaml-preamble
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension eliostruyf.vscode-front-matter # Markdown with front matter for Hugo, Jekyll, etc.
+
+# Ruby
+code --install-extension rebornix.ruby
+code --install-extension wingrunr21.vscode-ruby
+code --install-extension bung87.rails
+code --install-extension bung87.vscode-gemfile
+code --install-extension castwide.solargraph
+code --install-extension KoichiSasada.vscode-rdbg
+
+# JavaScript, TypeScript, and related frameworks
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension donjayamanne.jquerysnippets
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension svelte.svelte-vscode
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension eg2.vscode-npm-script
+
+# HTML
+code --install-extension abusaidm.html-snippets
+code --install-extension sidthesloth.html5-boilerplate
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+
+# CSS, SASS, Stylus, etc.
+code --install-extension syler.sass-indented
+code --install-extension sysoev.language-stylus
+
+# XML, YAML, TOML, etc.
+code --install-extension DotJoshJohnson.xml
+
+# Elixir
+code --install-extension mjmcloug.vscode-elixir
+
+# Crystal
+code --install-extension faustinoaq.crystal-lang
+
+# Nim
+code --install-extension kosz78.nim
+
+# MySQL
+code --install-extension formulahendry.vscode-mysql
+
+# DevOps
+code --install-extension shanoor.vscode-nginx
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension hashicorp.terraform
+
+# C, C++
+code --install-extension austin.code-gnu-global
+
+# Java
+code --install-extension redhat.java
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
+# TODO: Kotlin
+
+# Haskell, Elm, OCAML, ReasonML
+code --install-extension ocamllabs.ocaml-platform
+code --install-extension freebroccolo.reasonml
+code --install-extension justusadam.language-haskell
+code --install-extension sbrink.elm
+
+# Lisp, Scheme, Racket
+code --install-extension evzen-wybitul.magic-racket
+
+# Swift
+code --install-extension Kasik96.swift
