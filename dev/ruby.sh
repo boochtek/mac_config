21385a59f7a2d95a0c06dec4eb9b78f8bf249897
Craig Buchek <craig@boochtek.com>
2022-02-23 12:50:45 -0800

    Update Ruby versions

--- dev/ruby.sh
+++ dev/ruby.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+RUBY_VERSION="${RUBY_VERSION:-3.1.1}"
+RUBY2_VERSION="${RUBY2_VERSION:-2.7.5}"
+
 # Dependencies: `asdf`
 
 ## Install and configure various versions of Ruby.
 asdf plugin update ruby
-asdf install ruby 3.0.1
-asdf install ruby 2.7.3
+asdf install ruby "$RUBY_VERSION"
+asdf install ruby "$RUBY2_VERSION"
 # TODO: See if there are any old versions we want to get rid of. Maybe ask?
 #           Use `asdf uninstall ruby 2.7.1`
 #           Maybe set an `at` to remind you to run the script again?
@@ -15,7 +18,7 @@ asdf install ruby 2.7.3
 
 # NOTE: The asdf Ruby plugin installs all the gems listed in `~/.default-gems`
 # Install some gems for every version of Ruby. These are either for irb or for the commands they provide,
-GEMS_TO_INSTALL='bundler awesome_print hirb wirble pry railties rubocop'
+GEMS_TO_INSTALL='bundler amazing_print hirb wirble pry railties rubocop debug'
 ALL_RUBY_VERSIONS="$(asdf list ruby | tr -d '\n')"
 CURRENT_RUBY_VERSION=$(asdf current ruby | awk '{print $2}' | tr -d '\n')
 if [[ -z "$CURRENT_RUBY_VERSION" ]]; then
