beafe324589672eaa049a4f77403c0ad0cc69814
Craig Buchek <craig.buchek@truelinkfinancial.com>
2022-06-14 12:31:14 -0700

    Install and use dockutil 3.x
    Version 2.x of dockutil no longer works. It's trying to load /usr/bin/python,
    but there is no longer any Python 2 on the system. Trying to force it to use
    Python 3 did not work.
    
    Had to install "manually", as Homebrew does not yet include dockutil 3.x.

--- os/dock.sh
+++ os/dock.sh
@@ -10,15 +10,20 @@ defaults write com.apple.dock autohide -bool TRUE
 
 # We use dockutil to add and remove icons in the Mac OS X dock.
 ## Install dockutil
-brew install --quiet dockutil
+# NOTE: Homebrew doesn't have dockutil 3.0 yet, as of 2022-05-28.
+#brew install --quiet dockutil
+# We're installing manually, until Homebrew gets 3.0.
+wget https://github.com/kcrawford/dockutil/releases/download/3.0.2/dockutil-3.0.2.pkg
+sudo installer -pkg dockutil-3.0.2.pkg -target /
+rm -rf dockutil-3.0.2.pkg
 
 ## Remove rarely-used Dock items.
 for dock_item in Siri Launchpad Contacts Notes Reminders Maps Messages FaceTime iBooks Podcasts TV ; do
-    dockutil --remove "$dock_item" 1>/dev/null
+    dockutil --remove "$dock_item" > /dev/null
 done
 
 ## Add Activity Monitor to the Dock.
-dockutil --find "Activity Monitor" 1>/dev/null \
+dockutil --find "Activity Monitor" > /dev/null \
     || dockutil --add "/System/Applications/Utilities/Activity Monitor.app" --position end
 
 ## Restart the Dock.
