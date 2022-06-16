5336eca0e4af1175e50e30d4acc67ca15b722526
Craig Buchek <craig.buchek@truelinkfinancial.com>
2022-06-15 23:53:12 -0700

    Install several apps via App Store

---
+++ os/app_store.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Show Debug menu in App Store app.
+defaults write com.apple.appstore 'ShowDebugMenu' -bool TRUE
+
+## Install MAS (Mac App Store CLI) utility and apps that have been installed through App Store.
+## NOTE: You may be prompted (via GUI) to sign into the Mac App Store.
+
+# Install MAS.
+brew install mas
+
+# Install Magnet window manager. Start it at login and now.
+if [[ ! -e '/Applications/Magnet.app' ]]; then
+    mas list | grep -q 'Magnet' || mas lucky 'Magnet'
+    osascript -e 'tell application "System Events" to make login item with properties {path:"/Applications/Magnet.app", hidden:true}' >/dev/null
+    open -gj -a 'Magnet'
+fi
+
+# Install Collective clipboard manager. Start it at login and now.
+if [[ ! -e '/Applications/Collective.app' ]]; then
+    mas list | grep -q 'Collective' || mas lucky 'Collective'
+    osascript -e 'tell application "System Events" to make login item with properties {path:"/Applications/Collective.app", hidden:true}' >/dev/null
+    open -gj -a 'Collective'
+fi
+
+# Install 1Password password manager. Start it at login and now.
+if [[ ! -e '/Applications/1Password 7.app' ]]; then
+    mas list | grep -q '1Password 7' || mas lucky '1Password 7'
+    osascript -e 'tell application "System Events" to make login item with properties {path:"/Applications/1Password 7.app", hidden:true}' >/dev/null
+    open -gj -a '1Password 7'
+fi
+
+# Install Bear note-taking app. Start it at login and now.
+if [[ ! -e '/Applications/Bear.app' ]]; then
+    mas list | grep -q 'Bear' || mas lucky 'Bear'
+    osascript -e 'tell application "System Events" to make login item with properties {path:"/Applications/Bear.app", hidden:false}' >/dev/null
+    open -gj -a 'Bear'
+fi
+
+# Install Firetask task-tracking app. Start it at login and now.
+if [[ ! -e '/Applications/Firetask.app' ]]; then
+    mas list | grep -q 'Firetask' || mas lucky 'Firetask'
+    osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Firetask.app", hidden:true}' >/dev/null
+    open -gj -a 'Firetask'
+fi
+
+# TODO: More apps from the App Store.
+# TODO: What else should we auto-start?
+# TODO: Add items to Dock, as appropriate. (Make sure we've installed dockutils before this gets run.)
