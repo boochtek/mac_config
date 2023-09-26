3a625046dc3a2e4aae482f159554d69ec1603767
Craig Buchek <craig@boochtek.com>
2023-09-25 23:58:47 -0500

    Install Helix editor
    Note that the command is `hex`,

---
+++ editors/hex.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## Install [Helix Editor](https://helix-editor.com/).
+# Helix is terminal-only, run via the `hx` command.
+# Helix is inspired by NeoVim, with a lot of nice built-in features.
+brew install --quiet helix
