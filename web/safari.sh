e90a6f2efc54fe7b02087ad5295ad6f13588856a
Craig Buchek <craig@boochtek.com>
2023-11-26 12:45:21 -0600

    Safari: Several small updates
    - Don't send search queries to Apple
    - Disable Java in the browser
    - Enable all development settings

--- web/safari.sh
+++ web/safari.sh
@@ -1,4 +1,19 @@
 # safari.sh
 
 # Show the status bar at the bottom, so we can see URLs when we hover over links.
-defaults write com.apple.Safari 'ShowStatusBar' -bool true
+defaults write com.apple.Safari 'ShowStatusBar' -bool TRUE
+
+# Don’t send search queries to Apple. From https://github.com/sobolevn/dotfiles/blob/master/macos/settings.sh
+defaults write com.apple.Safari 'UniversalSearchEnabled' -bool FALSE
+defaults write com.apple.Safari 'SuppressSearchSuggestions' -bool TRUE
+
+# Disable Java.
+defaults write com.apple.Safari 'com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled' -bool FALSE
+defaults write com.apple.Safari 'com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles' -bool FALSE
+
+# Enable all the development settings.
+defaults write com.apple.Safari 'IncludeInternalDebugMenu' -bool TRUE
+defaults write com.apple.Safari 'IncludeDevelopMenu' -bool TRUE
+defaults write com.apple.Safari 'WebKitDeveloperExtrasEnabledPreferenceKey' -bool TRUE
+defaults write com.apple.Safari 'com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled' -bool TRUE
+defaults write NSGlobalDomain 'WebKitDeveloperExtras' -bool TRUE
