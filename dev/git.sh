40fae0c699cdf39e10f4e96e0daa2cb366367ea1
Craig Buchek <craig@boochtek.com>
2024-07-05 16:17:41 -0700

    Git: Install LFS properly; add oauth credential helper; delta for diffs

--- dev/git.sh
+++ dev/git.sh
@@ -9,14 +9,20 @@ brew link git
 brew install --quiet git-crypt
 
 # Git-LFS allows storing larger files. See https://git-lfs.com/.
-# GitHub limits files to 100 MB. You can add on LFS for $5/month for 50 GB, with a file size limit of 2 GB.
+# GitHub limits files to 100 MB. You can add LFS for $5/month for 50 GB, with a file size limit of 2 GB.
 brew install --quiet git-lfs
+git lfs install
 
 # LazyGit provides a TUI that makes some git operations easier.
 brew install --quiet lazygit
+
+# Install some credential helpers. NOTE: Prompts for sudo password.
+brew install --quiet --cask --no-quarantine git-credential-manager
+brew install --quiet git-credential-oauth
 
 # Suggested by Olivier Lacan https://twitter.com/olivierlacan/status/646741176922587141
 git config --global credential.helper osxkeychain
 
 # Make diffs more human-readable.
 brew install --quiet diff-so-fancy
+brew install --quiet git-delta
