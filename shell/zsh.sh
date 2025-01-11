331de0bb7421547ee9eea6c3fea75e803803cb2a
Craig Buchek <craig@boochtek.com>
2025-01-10 22:33:21 -0800

    Zsh: Add completions, syntax highlighting, auto-pair

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -3,5 +3,14 @@
 # MacOS comes with Zsh, but Homebrew usually has an updated version.
 brew install --quiet zsh
 
+# Additional completion definitions
+brew install --quiet zsh-completions
+
 # Show suggestions for full command lines, like Fish. Use cursor-right to accept.
 brew install --quiet zsh-autosuggestions
+
+# Automatically close brackets, quotes, etc.
+brew install --quiet zsh-autopair
+
+# Syntax highlighting as you type.
+brew install --quiet zsh-syntax-highlighting
