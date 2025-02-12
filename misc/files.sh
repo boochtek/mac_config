30b874bc5d8ea1db42634d552b1efcd3f5228020
Craig Buchek <craig@boochtek.com>
2025-02-11 22:34:56 -0800

    Install various TUI file managers
    - Midnight Commander (mc)
    - NNN
    - Ranger (Vim key bindings)

---
+++ misc/files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## File management tools.
+
+# Midnight Commander (mc) is a visual (TUI) file manager, a clone of Norton Commander.
+brew install midnight-commander
+
+# NNN is another TUI file manager, but with a different design philosophy.
+brew install --quiet nnn
+
+# Ranger is a TUI file manager with VI key bindings.
+brew install --quiet ranger
