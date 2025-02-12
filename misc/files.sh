4576dc5efbe2ebdd8e789ab782c3329fec8de53e
Craig Buchek <craig@boochtek.com>
2025-02-11 22:46:02 -0800

    Install file conversion tools
    - Poppler
        - pdfinfo, pdftotext, pdftohtml, etc
        - Forked from xpdf, but has a more acceptable set of dependencies
            - Includes some X11 libs, but not Qt
    - Tika
        - Can convert "thousands" of file types
        - Can do OCR
        - Requires Java
            - Installs OpenJDK
    - MarkItDown
        - Tool from Microsoft
        - Converts Word, PowerPoint, Excel, PDF, HTML, etc to Markdown

--- misc/files.sh
+++ misc/files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## File management tools.
+## Tools to work with files and file formats.
 
 # Midnight Commander (mc) is a visual (TUI) file manager, a clone of Norton Commander.
 brew install midnight-commander
@@ -10,3 +10,14 @@ brew install --quiet nnn
 
 # Ranger is a TUI file manager with VI key bindings.
 brew install --quiet ranger
+
+
+# Installs `pdfinfo`, `pdftotext`, and `pdftohtml`, among others.
+# Forked from xpdf, but has a different (large, including X11 libs) set of dependencies that I find preferable.
+brew install --quiet poppler
+
+# NOTE: Requires Java (installs openjdk) but can do OCR and convert "thousands" of file types.
+brew install --quiet tika
+
+# MarkItDown is a tool from Microsoft that converts Word, PowerPoint, Excel, PDF, HTML and other files to Markdown.
+pip install --quiet markitdown
