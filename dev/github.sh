e98fce2ab3828584826fda80f319d72cad6fb61a
Craig Buchek <craig@boochtek.com>
2025-02-13 23:05:27 -0800

    gh: log into GitHub to install extension

--- dev/github.sh
+++ dev/github.sh
@@ -10,5 +10,14 @@ trap 'RC=$? ; echo "$0: Error on line "$LINENO": $BASH_COMMAND" ; exit $RC' ERR
 # GitHub command line tool.
 brew install --quiet gh
 
+# NOTE: This will hang until you enter the one-time code in GitHub.
+# Authenticate with GitHub, if we have not already.
+if ! gh auth status > /dev/null 2>&1 ; then
+    gh config set prompt disabled
+    open https://github.com/login/device &
+    gh auth login
+fi
+
+# NOTE: You have to be authenticated to install extensions.
 # GitHub Copilot extension for gh. Use `gh copilot suggest` or `gh copilot explain` to use it.
-gh extension install github/gh-copilot
+gh extension install github/gh-copilot 2> /dev/null
