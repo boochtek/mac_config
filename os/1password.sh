ae1ce43f6c045aac040c1543f889995a1a6b6532
Craig Buchek <craig@boochtek.com>
2025-02-13 23:29:54 -0800

    1Password: install Safari extension

--- os/1password.sh
+++ os/1password.sh
@@ -3,7 +3,19 @@
 brew install --quiet --cask --no-quarantine 1password
 brew install --quiet 1password-cli
 
-# TODO: Install browser extensions.
+# TODO: Use this for virtualOS too.
+mas_install() {
+    local app_name="$1"
+    local app_id
+    app_id="$(mas search "${app_name}" | grep "${app_name}" | awk '{ print $1 }')"
+    mas purchase "${app_id}"
+    mas install "${app_id}"
+}
+
+mas_install '1Password for Safari'
+
+
+# TODO: Install other browser extensions.
 # TODO: Document using 1Password's SSH agent.
 # TODO: Integrate CLI. Figure out where it got installed to.
 # TODO: Allow CLI to use Touch ID.
